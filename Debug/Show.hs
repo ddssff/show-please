a438a84a3d48a40ce1b58fa0f22165b84180303b
David Fox <ddssff@gmail.com>
2016-06-13 06:12:39 -0700

    Rename showplease -> show-exception

--- Debug/Show.hs
+++ Debug/Show.hs
@@ -1,145 +1,108 @@
-{-# LANGUAGE CPP, FlexibleContexts, FlexibleInstances, BangPatterns, NoImplicitPrelude, StandaloneDeriving,
-             MagicHash, UnboxedTuples, UndecidableInstances #-}
-{-# OPTIONS_GHC -Wall -fno-warn-orphans #-}
--- | A wrapper type 'V' which has alternative instances of Show for
--- certain types whose official Show instances produce strings that
--- are not valid Haskell expressions.  For example:
--- @
---   > :m +System.IO.Error GHC.IO.Exception
---   > show (userError "hello")
---   user error (hello)
---   > show (V (userError "hello"))
---   "IOError {ioe_handle = Nothing, ioe_type = UserError, ioe_location = \"\", ioe_description = \"foo\", ioe_errno = Nothing, ioe_filename = Nothing}"
---   > putStrLn (show (V (userError "hello")))
---   IOError {ioe_handle = Nothing, ioe_type = UserError, ioe_location = "", ioe_description = "foo", ioe_errno = Nothing, ioe_filename = Nothing}
---   > IOError {ioe_handle = Nothing, ioe_type = UserError, ioe_location = "", ioe_description = "foo", ioe_errno = Nothing, ioe_filename = Nothing}
---   user error (hello)
--- @
+-- | A wrapper type and associated Show instances that generate
+-- correct haskell code, especially for exception types.
 
-module Debug.Show
-    ( Please.Show(..)
-    , V(V)
-    ) where
+{-# LANGUAGE FlexibleInstances #-}
+
+module Debug.Show (V(V)) where
 
 import Control.Exception
 import Data.Maybe
-import GHC.Base
 import Foreign.C.Types
+import GHC.Base
 import GHC.IO.Exception
 import GHC.IO.Handle
 import GHC.Show
-import qualified Debug.CopyOfShow as Please
 import Text.Parsec.Error
 import Text.Parsec.Pos
 
 newtype V a = V a
 
-instance Eq a => Eq (V a) where
-    (V a) == (V b) = a == b
-
--- Slip in our alternative show to show something wrapped in V.
-instance Please.Show a => Show (V a) where
-    show (V a) = Please.show a
-
--- Alternative show instances
-
-instance Please.Show CInt where show = show
-instance Please.Show Handle where show = show
-
-instance Please.Show IOException where
-    show e =
+instance Show (V IOException) where
+    show (V e) =
         "IOError {" ++
-          "ioe_handle = " ++ Please.show (ioe_handle e) ++ ", " ++
-          "ioe_type = " ++ Please.show (ioe_type e) ++ ", " ++
-          "ioe_location = " ++ Please.show (ioe_location e) ++ ", " ++
-          "ioe_description = " ++ Please.show (ioe_description e) ++ ", " ++
-          "ioe_errno = " ++ Please.show (ioe_errno e) ++ ", " ++
-          "ioe_filename = " ++ Please.show (ioe_filename e) ++ "}"
+          "ioe_handle = " ++ show (ioe_handle e) ++ ", " ++
+          "ioe_type = " ++ show (V (ioe_type e)) ++ ", " ++
+          "ioe_location = " ++ show (ioe_location e) ++ ", " ++
+          "ioe_description = " ++ show (ioe_description e) ++ ", " ++
+          "ioe_errno = " ++ show (ioe_errno e) ++ ", " ++
+          "ioe_filename = " ++ show (ioe_filename e) ++ "}"
 
-instance Please.Show IOErrorType where
-    show AlreadyExists = "AlreadyExists"
-    show NoSuchThing = "NoSuchThing"
-    show ResourceBusy = "ResourceBusy"
-    show ResourceExhausted = "ResourceExhausted"
-    show EOF = "EOF"
-    show IllegalOperation = "IllegalOperation"
-    show PermissionDenied = "PermissionDenied"
-    show UserError = "UserError"
-    show UnsatisfiedConstraints = "UnsatisfiedConstraints"
-    show SystemError = "SystemError"
-    show ProtocolError = "ProtocolError"
-    show OtherError = "OtherError"
-    show InvalidArgument = "InvalidArgument"
-    show InappropriateType = "InappropriateType"
-    show HardwareFault = "HardwareFault"
-    show UnsupportedOperation = "UnsupportedOperation"
-    show TimeExpired = "TimeExpired"
-    show ResourceVanished = "ResourceVanished"
-    show Interrupted = "Interrupted"
+instance Show (V IOErrorType) where
+    show (V AlreadyExists) = "AlreadyExists"
+    show (V NoSuchThing) = "NoSuchThing"
+    show (V ResourceBusy) = "ResourceBusy"
+    show (V ResourceExhausted) = "ResourceExhausted"
+    show (V EOF) = "EOF"
+    show (V IllegalOperation) = "IllegalOperation"
+    show (V PermissionDenied) = "PermissionDenied"
+    show (V UserError) = "UserError"
+    show (V UnsatisfiedConstraints) = "UnsatisfiedConstraints"
+    show (V SystemError) = "SystemError"
+    show (V ProtocolError) = "ProtocolError"
+    show (V OtherError) = "OtherError"
+    show (V InvalidArgument) = "InvalidArgument"
+    show (V InappropriateType) = "InappropriateType"
+    show (V HardwareFault) = "HardwareFault"
+    show (V UnsupportedOperation) = "UnsupportedOperation"
+    show (V TimeExpired) = "TimeExpired"
+    show (V ResourceVanished) = "ResourceVanished"
+    show (V Interrupted) = "Interrupted"
 
--- instance Please.Show ControlFileError where show = show
+instance Show (V Message) where
+    show (V (SysUnExpect s)) = "SysUnExpect " ++ show s
+    show (V (UnExpect s)) = "UnExpect " ++ show s
+    show (V (Expect s)) = "Expect " ++ show s
+    show (V (Message s)) = "Message " ++ show s
 
-instance Please.Show ParseError where
-    show e = "newErrorMessage " ++ Please.show (errorPos e) ++ " " ++ Please.show (errorMessages e)
-
-instance Please.Show Message where
-    show (SysUnExpect s) = "SysUnExpect " ++ show s
-    show (UnExpect s) = "UnExpect " ++ show s
-    show (Expect s) = "Expect " ++ show s
-    show (Message s) = "Message " ++ show s
-
-instance Please.Show SourcePos where
-    show pos =
+instance Show (V SourcePos) where
+    show (V pos) =
         "(newPos " ++ show (sourceName pos) ++ " " ++ show (sourceLine pos) ++ " " ++ show (sourceColumn pos) ++ ")"
 
-instance Please.Show SomeException where
-    show e =
+instance Show (V SomeException) where
+    show (V e) =
         "toException (" ++
-            (maybeFrom (v (fromException e :: Maybe ArithException)) $
-             maybeFrom (v (fromException e :: Maybe ArrayException)) $
-             maybeFrom (v (fromException e :: Maybe AssertionFailed)) $
-             maybeFrom (v (fromException e :: Maybe AsyncException)) $
-             maybeFrom (v (fromException e :: Maybe BlockedIndefinitelyOnMVar)) $
-             maybeFrom (v (fromException e :: Maybe BlockedIndefinitelyOnSTM)) $
-             maybeFrom (v (fromException e :: Maybe Deadlock)) $
-             -- maybeFrom (v (fromException e :: Maybe Dynamic)) $
-             maybeFrom (v (fromException e :: Maybe ErrorCall)) $
-             maybeFrom (v (fromException e :: Maybe ExitCode)) $
-             maybeFrom (v (fromException e :: Maybe IOException)) $
-             maybeFrom (v (fromException e :: Maybe NestedAtomically)) $
-             maybeFrom (v (fromException e :: Maybe NoMethodError)) $
-             maybeFrom (v (fromException e :: Maybe NonTermination)) $
-             maybeFrom (v (fromException e :: Maybe PatternMatchFail)) $
-             maybeFrom (v (fromException e :: Maybe RecConError)) $
-             maybeFrom (v (fromException e :: Maybe RecSelError)) $
-             maybeFrom (v (fromException e :: Maybe RecUpdError)) $
-             maybeFrom (v (fromException e :: Maybe SomeAsyncException)) $
-             ("No Debug.Show instance for " ++ show e)) ++ ")"
-
+            (maybeFrom (fmap ((++ " :: ArithException") . show . V) (fromException e :: Maybe ArithException)) $
+             maybeFrom (fmap ((++ " :: ArrayException") . show . V) (fromException e :: Maybe ArrayException)) $
+             maybeFrom (fmap ((++ " :: AssertionFailed") . show . V) (fromException e :: Maybe AssertionFailed)) $
+             maybeFrom (fmap ((++ " :: AsyncException") . show . V) (fromException e :: Maybe AsyncException)) $
+             maybeFrom (fmap ((++ " :: BlockedIndefinitelyOnMVar") . show . V) (fromException e :: Maybe BlockedIndefinitelyOnMVar)) $
+             maybeFrom (fmap ((++ " :: BlockedIndefinitelyOnSTM") . show . V) (fromException e :: Maybe BlockedIndefinitelyOnSTM)) $
+             maybeFrom (fmap ((++ " :: Deadlock") . show . V) (fromException e :: Maybe Deadlock)) $
+             -- maybeFrom (fmap ((++ " :: Dynamic") . show . V) (fromException e :: Maybe Dynamic)) $
+             maybeFrom (fmap ((++ " :: ErrorCall") . show . V) (fromException e :: Maybe ErrorCall)) $
+             maybeFrom (fmap ((++ " :: ExitCode") . show . V) (fromException e :: Maybe ExitCode)) $
+             maybeFrom (fmap ((++ " :: IOException") . show . V) (fromException e :: Maybe IOException)) $
+             maybeFrom (fmap ((++ " :: NestedAtomically") . show . V) (fromException e :: Maybe NestedAtomically)) $
+             maybeFrom (fmap ((++ " :: NoMethodError") . show . V) (fromException e :: Maybe NoMethodError)) $
+             maybeFrom (fmap ((++ " :: NonTermination") . show . V) (fromException e :: Maybe NonTermination)) $
+             maybeFrom (fmap ((++ " :: PatternMatchFail") . show . V) (fromException e :: Maybe PatternMatchFail)) $
+             maybeFrom (fmap ((++ " :: RecConError") . show . V) (fromException e :: Maybe RecConError)) $
+             maybeFrom (fmap ((++ " :: RecSelError") . show . V) (fromException e :: Maybe RecSelError)) $
+             maybeFrom (fmap ((++ " :: RecUpdError") . show . V) (fromException e :: Maybe RecUpdError)) $
+             maybeFrom (fmap ((++ " :: SomeAsyncException") . show . V) (fromException e :: Maybe SomeAsyncException)) $
+             ("No Show instance for V " ++ show e ++ ", report this as a bug at https://github.com/seereason/showexception")) ++ ")"
+ 
 -- I don't know exactly what these do - they may need to be implemented
 -- in more detail, but at least we can tell they name of their type now.
-instance Please.Show ArithException where show e = "ArithException: " ++ show e
-instance Please.Show ArrayException where show e = "ArrayException: " ++ show e
-instance Please.Show AssertionFailed where show e = "AssertionFailed: " ++ show e
-instance Please.Show AsyncException where show e = "AsyncException: " ++ show e
-instance Please.Show BlockedIndefinitelyOnMVar where show e = "BlockedIndefinitelyOnMVar: " ++ show e
-instance Please.Show BlockedIndefinitelyOnSTM where show e = "BlockedIndefinitelyOnSTM: " ++ show e
-instance Please.Show Deadlock where show e = "Deadlock: " ++ show e
--- instance Please.Show Dynamic where show e = "Dynamic: " ++ show e
-instance Please.Show ErrorCall where show e = "ErrorCall: " ++ show e
-instance Please.Show ExitCode where show e = "ExitCode: " ++ show e
--- instance Please.Show IOException where show e = "IOException: " ++ show e -- defined above
-instance Please.Show NestedAtomically where show e = "NestedAtomically: " ++ show e
-instance Please.Show NoMethodError where show e = "NoMethodError: " ++ show e
-instance Please.Show NonTermination where show e = "NonTermination: " ++ show e
-instance Please.Show PatternMatchFail where show e = "PatternMatchFail: " ++ show e
-instance Please.Show RecConError where show e = "RecConError: " ++ show e
-instance Please.Show RecSelError where show e = "RecSelError: " ++ show e
-instance Please.Show RecUpdError where show e = "RecUpdError: " ++ show e
-instance Please.Show SomeAsyncException where show e = "SomeAsyncException: " ++ show e
-
-v :: (Please.Show a, Functor f) => f a -> f String
-v = fmap Please.show
+instance Show (V ArithException) where show (V e) = "ArithException: " ++ show e
+instance Show (V ArrayException) where show (V e) = "ArrayException: " ++ show e
+instance Show (V AssertionFailed) where show (V e) = "AssertionFailed: " ++ show e
+instance Show (V AsyncException) where show (V e) = "AsyncException: " ++ show e
+instance Show (V BlockedIndefinitelyOnMVar) where show (V e) = "BlockedIndefinitelyOnMVar: " ++ show e
+instance Show (V BlockedIndefinitelyOnSTM) where show (V e) = "BlockedIndefinitelyOnSTM: " ++ show e
+instance Show (V Deadlock) where show (V e) = "Deadlock: " ++ show e
+-- instance Show (V Dynamic) where show (V e) = "Dynamic: " ++ show e
+instance Show (V ErrorCall) where show (V (ErrorCall s)) = "ErrorCall " ++ show s
+instance Show (V ExitCode) where show (V e) = "ExitCode: " ++ show e
+-- instance Show (V IOException) where show (V e) = "IOException: " ++ show e -- defined above
+instance Show (V NestedAtomically) where show (V e) = "NestedAtomically: " ++ show e
+instance Show (V NoMethodError) where show (V e) = "NoMethodError: " ++ show e
+instance Show (V NonTermination) where show (V e) = "NonTermination: " ++ show e
+instance Show (V PatternMatchFail) where show (V e) = "PatternMatchFail: " ++ show e
+instance Show (V RecConError) where show (V e) = "RecConError: " ++ show e
+instance Show (V RecSelError) where show (V e) = "RecSelError: " ++ show e
+instance Show (V RecUpdError) where show (V e) = "RecUpdError: " ++ show e
+instance Show (V SomeAsyncException) where show (V e) = "SomeAsyncException: " ++ show e
 
 maybeFrom :: Maybe c -> c -> c
 maybeFrom = flip fromMaybe
--- README.md
+++ README.md
@@ -1,45 +1,32 @@
 Debugging Tools
 ===============
 
-Debug.Show
+System.ShowBetter
 ----------
 
-This module exports a class Show, which is a duplicate of the
-standard system Show, along with a collection of Please.Show instances
+This module exports a type V(V), along with a collection of Show instances
 (initially for the types in Control.Exception) that actually display
 the values as a Haskell expression, so that they can be fully
-understood and used by the Read class.  There is also a wrapper type V
-which is used to cause the regular show to use the Debug.Show instance:
+understood and used by the Read class.
 
-    > Prelude.show (userError "foo")
+    > show (userError "foo")
     "user error (foo)"
-    > Debug.Show.show (userError "foo")
+    > show (V (userError "foo"))
     "IOError {ioe_handle = Nothing, ioe_type = UserError, ioe_location = \"\", ioe_description = \"foo\", ioe_errno = Nothing, ioe_filename = Nothing}"
 
 Some exception types have less detailed Show implementations, but at
 least they include the type name:
 
-    > Prelude.show (SomeException (ErrorCall "abc"))
-    "abc"
-    > Debug.Show.show (SomeException (ErrorCall "abc"))
-    "toException (ErrorCall: abc)"
+    > putStrLn (show (SomeException (ErrorCall "abc")))
+    abc
+    > putStrLn (show (V (SomeException (ErrorCall "abc"))))
+    toException (ErrorCall: abc :: ErrorCall)
 
-Debug.Loc
----------
+Now you can see what the real type of a SomeException is:
 
-This module exports __LOC__, a template haskell function which expands
-to an expression containing file location information about the place
-it was used.  The expression of type Loc from module
-Language.Haskell.TH.Syntax.  It may be used for constructing error
-messages, e.g.
-
-    throw $ MyException $__LOC__
-
-(Submitted for inclusion in https://github.com/gregwebs/FileLocation.hs)
-
-Debug.Console
--------------
-This module exports ePutStr and ePutStrLn, which atomically write
-messages to standard error.  A multi-threaded program can call these
-to write debugging output to the console without the messages getting
-garbled together by concurrency.
+    > error "foo" `catch` (\(e :: SomeException) -> pure (show (V e))) >>= putStrLn
+    toException (ErrorCall "foo" :: ErrorCall)
+    > readProcess "false" [] "" `catch` (\(e :: SomeException) -> pure (show (V e))) >>= putStrLn
+    .toException (IOError {ioe_handle = Nothing, ioe_type = OtherError, ioe_location = "readCreateProcess: false (exit 1)", ioe_description = "", ioe_errno = Nothing, ioe_filename = Nothing} :: IOException)
+    > (forkIO (hClose stdout) >>= (putStrLn . show)) `catch` (\(e :: SomeException) -> putStrLn (show (V e)))
+    ghc: <stdout>: hFlush: illegal operation (handle is closed)
